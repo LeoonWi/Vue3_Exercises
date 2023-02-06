49dc0f2c75eb6d003af53f33cbace9ec6b3e9d1d
LeoonWi <redwind090@gmail.com>
2023-02-06 20:56:06 +0300

    Показ по условию

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,29 @@
   export default {
     data() {
       return {
-        count: 0,
+        visible: true,
       }
     },
     methods: {
-      once: function() {
-        return this.count++;
+      vis: function() {
+        this.visible = true;
+      },
+      unvis: function() {
+        this.visible = false;
       }
     }    
   }
 </script>
 
 <template>
-  <a href="https://youtube.com" @click.prevent>Youtube.com</a><br>
-  <button @click.once="once">Приветствие №{{ count }}</button>
+  <div>
+    <p v-if="visible">Первый абзац!</p>
+    <p v-if="!visible">Второй абзац!</p>
+  </div>
+  <div>
+    <button @click="vis" v-if="!visible">Показать первый абзац</button>
+    <button @click="unvis" v-if="visible">Показать второй абзац</button>
+  </div>
 </template>
 
 <style>
