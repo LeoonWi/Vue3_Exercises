502d5ab160aa34033df928bdd75fe8c987df0d81
LeoonWi <redwind090@gmail.com>
2023-02-06 19:30:01 +0300

    Вывод даты с помощью пары методов.

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,39 @@
   export default {
     data() {
       return {
-        num1: 5,
-        num2: 10,
+        num_day: null,
       }
     },
     methods: {
       show: function() {
-        alert(this.num1 + this.num2);
+        let date = new Date();
+        let Day = date.getDay();
+        let weekDay = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+        alert(weekDay[Day]);
+      },
+      now_day: function() {
+      
+        return weekDay[nDay];
+      },
+      numday: function() {
+        let Num_Day = this.day(this.num_day);
+        alert(Num_Day);
+      },
+      day: function(nDay) {
+        let weekDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота' ,'Воскресенье'];
+        return weekDay[nDay-1];
       }
     }
   }
 </script>
 
 <template>
-  {{ show() }}
+  <form action="" @submit="numday">
+    Введите номер недели:<br>
+    <input type="number" v-model="num_day" name="numday" min="1" max="7">
+    <button type="submit">Нажми на меня</button>
+  </form><br>
+  <button @click="show">Показать текущий день недели</button>
 </template>
 
 <style>
