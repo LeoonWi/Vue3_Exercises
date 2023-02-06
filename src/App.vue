70ad12bc0da4d99621c9b56cbc7206e706d91e6a
LeoonWi <redwind090@gmail.com>
2023-02-06 20:08:57 +0300

    Передача параметров в метод

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,25 @@
   export default {
     data() {
       return {
-        num_day: null,
+        num: '',
       }
     },
     methods: {
-      show: function() {
-        let date = new Date();
-        let Day = date.getDay();
-        let weekDay = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-        alert(weekDay[Day]);
-      },
-      now_day: function() {
-      
-        return weekDay[nDay];
-      },
-      numday: function() {
-        let Num_Day = this.day(this.num_day);
-        alert(Num_Day);
-      },
-      day: function(nDay) {
-        let weekDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота' ,'Воскресенье'];
-        return weekDay[nDay-1];
+      show: function(str) {
+        if (str == 'sqr') alert(Math.pow(this.num, 2));
+        else if (str == 'pwr3') alert(Math.pow(this.num, 3));
       }
     }
   }
 </script>
 
 <template>
-  <form action="" @submit="numday">
-    Введите номер недели:<br>
-    <input type="number" v-model="num_day" name="numday" min="1" max="7">
-    <button type="submit">Нажми на меня</button>
-  </form><br>
-  <button @click="show">Показать текущий день недели</button>
+  <div>
+    Введите число:<br>
+    <input type="text" v-model="num">
+  </div><br>
+  <button @click="show('sqr')">Возвести в квадрат</button><br>
+  <button @click="show('pwr3')">Возвести в третью степень</button>
 </template>
 
 <style>
