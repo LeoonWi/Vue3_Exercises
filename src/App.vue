ff681f3af33dcb182d10693b06cf264c444ccdff
LeoonWi <redwind090@gmail.com>
2023-03-20 00:12:30 +0300

    Перебор массива объектов

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,28 @@
   export default {
     data() {
       return {
-        items: [1, 2, 3],
+        hrefs: [
+		    	{href: '1.html', text: 'text1'},
+		    	{href: '2.html', text: 'text2'},
+		    	{href: '3.html', text: 'text3'},
+		    ],
+        products: [
+		    	{
+		    		name: 'product1',
+		    		price: 100,
+		    		quantity: 5
+		    	},
+		    	{
+		    		name: 'product2',
+		    		price: 200,
+		    		quantity: 4
+		    	},
+		    	{
+		    		name: 'product3',
+		    		price: 300,
+		    		quantity: 3
+		    	}
+        ],
       }
     },
     methods: {
@@ -12,21 +33,25 @@
 </script>
 
 <template>
-  <template v-for="elem in items">
-    <div>
-      <p>{{ elem }}</p>
-      <p class="devider"></p>
-    </div>
-  </template>
   <div>
-    <template v-for="elem in items">
-      <ul>
-        <li>{{ elem }}</li>
-        <li class="devider"></li>
-      </ul>
-    </template>
+    <ul>
+      <template v-for="elem in hrefs">
+        <li><a v-bind:href="elem.href">{{ elem.text }}</a></li>
+      </template>
+    </ul>    
   </div>
-</template>
+  <div>
+    <table>
+      <template v-for="elem in products">
+        <td>
+          <tr>{{ elem.name }}</tr>
+          <tr>{{ elem.price }}</tr>
+          <tr>{{ elem.quantity }}</tr>
+        </td>
+      </template>
+    </table>
+  </div>
+ </template>
 
 <style>
 
