03c192c2d87299c52160b5f8e4629c8895c1a193
LeoonWi <redwind090@gmail.com>
2023-03-19 16:24:29 +0300

    Сложные условия

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script>
   export default {
     data() {
+      let days = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'];
+      var d = new Date();
+      var n = d.getDay();
       return {
-        isAdmin: true,
+        day: days[n],
       }
     },
     methods: {
@@ -13,8 +16,13 @@
 
 <template>
   <div>
-    <p v-if="isAdmin">Админ</p>
-    <p v-else>Пользователь</p>
+    <p v-if="day === 'Воскресенье'">Воскресенье</p>
+    <p v-if="day === 'Понедельник'">Понедельник</p>
+    <p v-if="day === 'Вторник'">Вторник</p>
+    <p v-if="day === 'Среда'">Среда</p>
+    <p v-if="day === 'Четверг'">Четверг</p>
+    <p v-if="day === 'Пятница'">Пятница</p>
+    <p v-if="day === 'Суббота'">Суббота</p>
   </div>
 </template>
 
