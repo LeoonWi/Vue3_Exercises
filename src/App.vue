bea7f813d237ed6a1c2888044c66ed7fc5fb9625
LeoonWi <redwind090@gmail.com>
2023-03-20 00:15:08 +0300

    Цикл в диапазон

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,7 @@
   export default {
     data() {
       return {
-        hrefs: [
-		    	{href: '1.html', text: 'text1'},
-		    	{href: '2.html', text: 'text2'},
-		    	{href: '3.html', text: 'text3'},
-		    ],
-        products: [
-		    	{
-		    		name: 'product1',
-		    		price: 100,
-		    		quantity: 5
-		    	},
-		    	{
-		    		name: 'product2',
-		    		price: 200,
-		    		quantity: 4
-		    	},
-		    	{
-		    		name: 'product3',
-		    		price: 300,
-		    		quantity: 3
-		    	}
-        ],
+        
       }
     },
     methods: {
@@ -34,22 +13,7 @@
 
 <template>
   <div>
-    <ul>
-      <template v-for="elem in hrefs">
-        <li><a v-bind:href="elem.href">{{ elem.text }}</a></li>
-      </template>
-    </ul>    
-  </div>
-  <div>
-    <table>
-      <template v-for="elem in products">
-        <td>
-          <tr>{{ elem.name }}</tr>
-          <tr>{{ elem.price }}</tr>
-          <tr>{{ elem.quantity }}</tr>
-        </td>
-      </template>
-    </table>
+    <p v-for="elem in 30">{{ elem }}</p>
   </div>
  </template>
 
