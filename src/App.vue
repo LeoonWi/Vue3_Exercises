d21152f6457599569da04c8a56330c44c8e06cb5
LeoonWi <redwind090@gmail.com>
2023-03-21 20:11:42 +0300

    Реактивность массивов

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,48 @@
   export default {
     data() {
       return {
-        products: [
-		    	{
-		    		id: 1,
-		    		name: 'product1',
-		    	},
-		    	{
-		    		id: 2,
-		    		name: 'product2',
-		    	},
-		    	{
-		    		id: 3,
-		    		name: 'product3',
-		    	},
-		    ]
+        arr: [],
+        elem: 0,
       }
     },
     methods: {
-      
+      add: function() {
+        this.arr.push(++this.elem);
+      },
+      del_start: function() {
+        this.arr.shift();
+      },
+      del_pop: function() {
+        this.arr.pop();
+      },
+      del_pre_last: function() {
+        this.arr.splice(this.arr.length-2, 1);
+      },
+      sort: function() {
+        this.arr.sort();
+      },
+      sort_rev: function() {
+        this.arr.reverse();
+      }
     }    
   }
 </script>
 
 <template>
-  <ul>
-    <li v-for="elem in products" :key="elem.id">{{ elem.name }}</li>
-  </ul>
- </template>
+  <div>
+    <ul>
+      <li v-for="elem in arr">{{ elem }}</li>
+    </ul>    
+  </div>
+  <div>
+    <button @click="add">Добавить в конце</button>
+    <button @click="del_start">Удалить в начале</button>
+    <button @click="del_pop">Удалить в конце</button>
+    <button @click="del_pre_last">Удалить в предпоследний</button>
+    <button @click="sort">Осортировать</button>
+    <button @click="sort_rev">Осортировать в обратном порядке</button>
+  </div>
+</template>
 
 <style>
 
