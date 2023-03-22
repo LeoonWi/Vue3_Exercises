49ef3fa1d597b1020dbc39ab1f2657345931e993
LeoonWi <redwind090@gmail.com>
2023-03-22 03:27:55 +0300

    Получение данных формы

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,59 @@
   export default {
     data() {
       return {
-        msg: null,
-        num: null,
+        num: 0,
+        res1: 0,
+        num1: 0,
+        num2: 0,
+        txt1: '',
+        txt2: '',
+        fio: '',
+        arr: [],
       }
     },
     methods: {
-
+      sqr: function() {
+        this.res1 = this.num**2;
+      },
+      sum: function() {
+        this.res2 = this.num1 + this.num2;
+      },
+      txt: function() {
+        let temp = this.txt1;
+        this.txt1 = this.txt2;
+        this.txt2 = temp;
+      },
+      sep: function() {
+        this.arr = this.fio.split(' ');
+      }
     }
   }    
 </script>
 
 <template>
   <div>
-    <input v-model="msg"><br>
-    <p :style="{textTransform: 'uppercase'}">{{ msg }}</p>
+    <input v-model="num">
+    <button @click="sqr">Отправить</button><br>
+    <p>Ответ: {{ res1 }}</p>
   </div>
   <div>
-    <input v-model="num"><br>
-    <p>{{ num**2 }}</p>
+    <input v-model="num1">
+    <input v-model="num2">
+    <button @click="sqr">Отправить</button><br>
+    <p>Ответ: {{ res2 }}</p>
+  </div>
+  <div>
+    <input v-model="txt1">
+    <input v-model="txt2">
+    <button @click="txt">Отправить</button><br>
+    <p>Ответ: {{ txt1 }} и {{ txt2 }}</p>
+  </div>
+  <div>
+    <input v-model="fio">
+    <button @click="sep">Отправить</button><br>
+    <p>Ответ: {{ arr[0] }}</p>
+    <p>Ответ: {{ arr[1] }}</p>
+    <p>Ответ: {{ arr[2] }}</p>
   </div>
 </template>
 
