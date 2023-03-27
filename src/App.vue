19da1dd094a1c97b8769560c41058c421d95d9ef
LeoonWi <redwind090@gmail.com>
2023-03-27 16:29:54 +0300

    Работа с радиокнопками

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   export default {
     data() {
       return {
-        arr: [],
+        lang: null,
       }
     },
     methods: {
@@ -12,15 +12,16 @@
 </script>
 
 <template>
-  <p>Какие языки вы знаете?</p><br>
+  <p>Выберите язык</p><br>
   <div>
-    <label><input type="checkbox" v-model="arr" value="Английский"> Английский</label><br>
-    <label><input type="checkbox" v-model="arr" value="Французский"> Французский</label><br>
-    <label><input type="checkbox" v-model="arr" value="Русский"> Русский</label><br>
-  </div>
-  <ul>
-    <li v-for="elem in arr">{{ elem }}</li>
-  </ul>
+    <label><input type="radio" name="lang" v-model="lang" value="ru"> Русский</label><br>
+    <label><input type="radio" name="lang" v-model="lang" value="eng"> Английский</label><br>
+    <label><input type="radio" name="lang" v-model="lang" value="bin"> Машинный язык</label>
+  </div><br>
+  <p v-if="lang === 'ru'">Привет!</p>
+  <p v-if="lang === 'eng'">Hello!</p>
+  <p v-if="lang === 'bin'">0x55 0x89 0xe5 0xe8 0xfc 0xff 0xff 0xff 0x83 0xf8 0x41 0x75 0x0d 0x68 0x00 0x00 0x00 0x00 0xe8 0xfc 0xff 0xff 0xff 0x83 0xc4 0x04 0xb8 0x00 0x00 0x00 0x00 0x89 0xec 0x5d 0xc3</p>
+  
 </template>
 
 <style>
