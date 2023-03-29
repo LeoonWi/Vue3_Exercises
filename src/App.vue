ee12ad60cf510a554af0789aaf8bb39f72a7c788
LeoonWi <redwind090@gmail.com>
2023-03-29 03:29:07 +0300

    Форма для добавления элементов в массив

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,28 @@
   export default {
     data() {
       return {
+        arr: [],
         message: '',
-        actualmessage: '',
-        text: '',
-        text1: ''
       };
     },
     methods: {
-      submit: function(){
-        this.actualmessage = this.message
+      addElem: function() {
+        this.arr.push(this.message)
       },
-      show: function(){
-        this.text = 'ALT key is pressed!'
-      },
-      displayText(button) {
-        if (button === 'left') {
-            this.text1 = 'left'} 
-            else if (button === 'right') {
-            this.text1 = 'right'} 
-            else if (button === 'middle') {
-            this.text1 = 'middle'
-        }
+      addElemToStart: function() {
+        this.arr.unshift(this.message)
       }
     }
   }    
 </script>
 
 <template>
-    <input v-model="message" @keyup.enter="submit"/>
-        <p> {{ actualmessage  }}</p>
-
-        <a href="#" @click.alt="show" > ссылка </a>
-        <p>{{ text }}</p>
-
-
-        <a href="#" @click.left="displayText('left')" @click.right="displayText('right')" @click.middle="displayText('middle')">Click me!</a>
-        <p>{{ text1 }}</p>
+    <input type="text" v-model="message"><br>
+    <button @click="addElemToStart">Добавить новый элемен в начало</button><br>
+    <button @click="addElem">Добавить новый элемен в конец</button>
+    <ul>
+      <li v-for="(elem, index) in arr" :key="index">{{ elem }}</li>
+    </ul>
 </template>
 
 <style>
