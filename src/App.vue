b06d512d8a4a382a1c4332a45b620fb329f0c2b6
LeoonWi <redwind090@gmail.com>
2023-03-29 03:57:37 +0300

    Форма редактирования

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,21 @@
 		    		name: 'name1',
 		    		salary: 100,
 		    		age: 30,
+            edit: false,
 		    	},
 		    	{
 		    		id: 2,
 		    		name: 'name2',
 		    		salary: 200,
 		    		age: 40,
+            edit: false,
 		    	},
 		    	{
 		    		id: 3,
 		    		name: 'name3',
 		    		salary: 300,
 		    		age: 50,
+            edit: false,
 		    	},
 		    ],
       };
@@ -29,6 +32,12 @@
         this.users = this.users.filter((user) => {
           return user.id != id;
         })
+      },
+      editUser: function(user) {
+        user.edit = true;
+      },
+      saveUser: function(user) {
+        user.edit = false;
       }
     }
   }    
@@ -37,17 +46,34 @@
 <template>
   <table>
     <tr v-for="user in users" :key="user.id">
-      <td>
-        {{  user.name }}
-      </td>
-      <td>
-        {{ user.salary }}
-      </td>
-      <td>
-        {{ user.age }}
-      </td>
+      <template v-if="!user.edit">
+        <td>
+          {{  user.name }}
+        </td>
+        <td>
+          {{ user.salary }}
+        </td>
+        <td>
+          {{ user.age }}
+        </td>
+      </template>
+      <template v-if="user.edit">
+        <td>
+          <input type="text" v-model="user.name">
+        </td>
+        <td>
+          <input type="text" v-model="user.salary">
+        </td>
+        <td>
+          <input type="text" v-model="user.age">
+        </td>
+      </template>
       <td>
         <button @click="delUser(user.id)">Удалить пользователя</button>
+      </td>
+      <td>
+        <button @click="editUser(user)" v-if="!user.edit">Редактировать пользователя</button>
+        <button @click="saveUser(user)" v-if="user.edit">Сохранить пользователя</button>
       </td>
     </tr>
   </table>
